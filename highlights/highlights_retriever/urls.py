ede89719e62ac181794be838c2af0e794dc61d53
rob wiggins <wigginsro11@gmail.com>
2020-08-09 10:53:14 -0400

    route for requesting clips from twitch that is wrong

--- highlights/highlights_retriever/urls.py
+++ highlights/highlights_retriever/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('clips/', views.get_clips, name='clips'),
+    path('', views.index, name='index'),
 ]
--- highlights/highlights_retriever/views.py
+++ highlights/highlights_retriever/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
+
+
 def index(request):
     return HttpResponse("Hello, world. You're at the highlights index.")
+
+
+def get_clips(request):
+    # topGamesRes = requests.get('https://api.twitch.tv/helix/games/top')
+    # gamesRes = requests.get('https://api.twitch.tv/helix/games')
+    clipsRes = requests.get('https://api.twitch.tv/helix/clips?name=Call%20of%20Duty%3A%20Modern%20Warfare')
+    print(clipsRes.json())
